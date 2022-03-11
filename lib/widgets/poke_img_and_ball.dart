68d6adb1f0d56e67993dc2ce332a99daeed30d42
mehmetgulcuxyz <mehmet.gulcu@atolyevizyon.com>
2022-03-11 22:13:19 +0300

    ffsfgf

---
+++ lib/widgets/poke_img_and_ball.dart
@@ -0,0 +1,45 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:pokedex_app/constants/ui_helper.dart';
+import 'package:pokedex_app/model/pokemon_model.dart';
+
+class PokeImageAndBall extends StatelessWidget {
+  final PokemonModel pokemon;
+
+  const PokeImageAndBall({Key? key, required this.pokemon}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    String pokeballImageUrl = 'images/pokeball.png';
+
+    return Stack(
+      children: [
+        Align(
+          alignment: Alignment.bottomRight,
+          child: Image.asset(
+            pokeballImageUrl,
+            width: UIHelper.calculatePokeImgAndBallSize(),
+            height: UIHelper.calculatePokeImgAndBallSize(),
+            fit: BoxFit.fitHeight,
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomRight,
+          child: Hero(
+            tag: pokemon.id!,
+            child: CachedNetworkImage(
+              errorWidget: (context, url, error) => Icon(Icons.ac_unit),
+              imageUrl: pokemon.img ?? '',
+              width: UIHelper.calculatePokeImgAndBallSize(),
+              height: UIHelper.calculatePokeImgAndBallSize(),
+              fit: BoxFit.fitHeight,
+              placeholder: (context, url) => CircularProgressIndicator(
+                color: Colors.red,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
